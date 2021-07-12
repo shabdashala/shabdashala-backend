d64650af4b11e7af40bf8bc1bc4dda9d34e7ae70
Bala Subrahmanyam Varanasi <vabasu@gmail.com>
2021-07-12 15:10:29 +0530

    feat: add jwt verify url

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -22,6 +22,7 @@ from . import views as accounts_views
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('registration/', include('dj_rest_auth.registration.urls')),
     path('facebook/', accounts_views.FacebookLogin.as_view(), name='fb_login'),
     path('', include('dj_rest_auth.urls')),
