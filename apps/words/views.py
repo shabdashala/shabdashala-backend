593eee4525744eac90adee2b7b274f43129799d3
Bala Subrahmanyam Varanasi <vabasu@gmail.com>
2021-07-23 00:40:12 +0530

    feat: add words views

--- apps/words/views.py
+++ apps/words/views.py
@@ -1,17 +1,21 @@
 from django.views import generic
 from django.contrib.auth import mixins as auth_mixins
 
+from . import models as word_models
+
 
 class WordIndexView(auth_mixins.LoginRequiredMixin, generic.TemplateView):
     template_name = 'words/index.html'
 
 
 class WordListView(auth_mixins.LoginRequiredMixin, generic.ListView):
-    template_name = 'words/index.html'
+    template_name = 'words/list.html'
+    model = word_models.Word
 
 
 class WordCreateView(auth_mixins.LoginRequiredMixin, generic.ListView):
-    template_name = 'words/index.html'
+    template_name = 'words/form.html'
+    model = word_models.Word
 
 
 class WordUpdateView(auth_mixins.LoginRequiredMixin, generic.ListView):
