d37f2dc70a227e000e20ee16a053cba901880c7c
Bala Subrahmanyam Varanasi <vabasu@gmail.com>
2021-07-25 03:44:10 +0530

    update

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,9 +1,18 @@
 from django.views import generic
 from django.contrib.auth import mixins as auth_mixins
 
+from apps.categories import models as categories_models
+
 
 class IndexView(auth_mixins.LoginRequiredMixin, generic.TemplateView):
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = categories_models.Category.objects.filter(
+            is_public=True, depth=1,
+        )
+        return context
 
 
 class AboutUsView(auth_mixins.LoginRequiredMixin, generic.TemplateView):
--- scripts/ONE_TIME/create_telugu_sandhulu.py
+++ scripts/ONE_TIME/create_telugu_sandhulu.py
@@ -1,4 +1,3 @@
-import copy
 import csv
 import os
 
