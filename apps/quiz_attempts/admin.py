0c1104f42037bb60337ada13be12abe88220b65f
Bala Subrahmanyam Varanasi <vabasu@gmail.com>
2021-08-04 16:12:37 +0530

    feat: add more list display items

--- apps/quiz_attempts/admin.py
+++ apps/quiz_attempts/admin.py
@@ -11,4 +11,5 @@ class AttemptedQuestionInline(admin.StackedInline):
 @admin.register(quiz_attempts_models.QuizAttempt)
 class QuizAttemptAdmin(admin.ModelAdmin):
     inlines = [AttemptedQuestionInline]
-    list_display = ['uuid', 'quiz']
+    list_display = ['quiz', 'user', 'is_completed', 'is_abandoned', 'is_deleted', 'date_removed']
+    list_filter = ['completed_at', 'is_completed', 'is_deleted', 'is_abandoned']
